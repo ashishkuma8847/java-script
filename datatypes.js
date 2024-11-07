681d4ed5ea5430dbc49527d1ee12dc6ec18a747d
ashishkuma8847 <ashishkuma8847@gmail.com>
2024-11-07 13:07:00 +0530

    ashishs

---
+++ datatypes.js
@@ -0,0 +1,54 @@
+// data types 
+// premitive,non premitive,
+// premitive
+// string
+let ashish = "56"
+console.log(typeof ashish);
+// number
+let ashish1 = 56
+console.log(typeof ashish1);
+// boolean
+let ashish2 = true
+console.log(typeof ashish2);
+// null
+let ashish3 = null
+console.log(typeof ashish3 );
+// null ko console karne par object aata hai
+// undifind
+let ashish4 
+console.log(ashish4 );
+// bigint larger number
+let ashish5 = 73724293798472749372348324832830498283048238428038483280438248n
+console.log(typeof ashish5);
+// symbol 
+// Symbol (unique and immutable)
+let uniqueId = Symbol('id');
+console.log(typeof uniqueId);
+
+
+// non premetive
+// object
+let ashish6 = {
+    username : "ashish"
+}
+console.log(typeof ashish6);
+// array
+let ashish7 =[
+      {
+        username : "ashish",
+        id : "ashish",
+        pass : "ashish"
+    }
+]
+console.log(typeof ashish7);
+// Function
+function sayHello() {
+    return 'Hello!';
+  }
+  
+
+
+
+
+
+
