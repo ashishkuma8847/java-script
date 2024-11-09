735141800851aecd291f941b88a296f6e7ec5069
ashishkuma8847 <ashishkuma8847@gmail.com>
2024-11-09 12:28:04 +0530

    ssa

--- datatypes.js
+++ datatypes.js
@@ -12,11 +12,11 @@ let ashish2 = true
 console.log(typeof ashish2);
 // null
 let ashish3 = null
-console.log(typeof ashish3 );
+console.log(typeof ashish3);
 // null ko console karne par object aata hai
 // undifind
-let ashish4 
-console.log(ashish4 );
+let ashish4
+console.log(ashish4);
 // bigint larger number
 let ashish5 = 73724293798472749372348324832830498283048238428038483280438248n
 console.log(typeof ashish5);
@@ -29,23 +29,23 @@ console.log(typeof uniqueId);
 // non premetive ek se jyada value ko hold karte hia
 // object
 let ashish6 = {
-    username : "ashish"
+  username: "ashish"
 }
 console.log(typeof ashish6);
 // array
-let ashish7 =[
-      {
-        username : "ashish",
-        id : "ashish",
-        pass : "ashish"
-    }
+let ashish7 = [
+  {
+    username: "ashish",
+    id: "ashish",
+    pass: "ashish"
+  }
 ]
 console.log(typeof ashish7);
 // Function
 function sayHello() {
-    return 'Hello!';
-  }
-  // Date
+  return 'Hello!';
+}
+// Date
 let currentDate = new Date();
 console.log(currentDate);
 
@@ -56,11 +56,11 @@ let ashish8 = "117861"
 ashish8 = Number("117861")
 console.log(typeof ashish8);
 // Coercion
-let ashish9 = "5"- 4
+let ashish9 = "5" - 4
 console.log(ashish9);
 
 
-let ashish10 = "5"+ 4
+let ashish10 = "5" + 4
 console.log(ashish10);
 // minus karne par ghta diya jayega par plus karne par dono ko sath mai likh diya jayega ye sirf + k sath hi hota hai baki * / - k sath esa kuch nhi hota 
 
