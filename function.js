c398d79610b5818f0b74ffa0052a42c47f42365f
ashishkuma8847 <ashishkuma8847@gmail.com>
2024-12-18 12:24:38 +0530

    xx

--- function.js
+++ function.js
@@ -1,10 +1,15 @@
 // JavaScript Functions
+// JAVASCRIPT MAIN FUNCTION EK BLOCK OF CODE HOTA HAI JO KI EK SPESIFIC TASK PERFOME KARTA HAI  AUR ESSE
+
+// RAGULAR OR NORMAL FUNCTION 
+// YEH SABSE BASIC TARIKA HIA  FUNCTION BANANE KA ISKA NAAM HOTA HAI OR ESE KHI BHI CALL KAR SATHE HAI
 // 1. Normal Function
 // A normal function is the most common way to define a function in JavaScript.
 
 // function sayHello() {
 //     console.log("Hello, world!");
 // }
+// sayHello()
 
 // 2. Arrow Function
 // An arrow function is a shorter way to write a function in JavaScript.
@@ -18,13 +23,35 @@
 
 // function greetUser(callback) {
 //     console.log("Greeting user...");
-//     callback();
-// }
+//     callback()
+//   }
 
-// function displayMessage() {
+//   function displayMessage() {
 //     console.log("This is a callback function!");
-// }
+//   }
+//   greetUser(displayMessage)
 
+// 4. annonymous
+  // es mai ham variable k ander function ko naam diye bina chla sakte hai 
+
+//    let ashish =  function(a,b)  {
+//     return a*b
+//    }
+//    console.log(ashish(2,3));
+   
+
+// 5. iife (immedeately invoked function expression)
+//   ye function turant exicue ho jata hai jaise hi difine hota hai
+
+// (function(){
+// console.log("iife");
+// })()
+
+// 6. CUNSTRUCTOR FUNCTION
+// objects ko create karne k liye use hota hai
+ 
+
+// greetUser(displayMessage)
 
 
 // Function Parameters and Return Values
@@ -34,7 +61,9 @@
 
 // function addNumbers(a, b) {
 //     console.log(a + b);
+    
 // }
+// addNumbers(1,2)
 
 // 2. Function with Return Value
 // A function can return a value, which can be used later in your code.
@@ -42,6 +71,11 @@
 // function multiplyNumbers(a, b) {
 //     return a * b;
 // }
+//     console.log(
+//         multiplyNumbers(2,2)
+
+//     );
+    
 
 
 
---
+++ stringmatheds.js
@@ -0,0 +1,14 @@
+// string mathods
+
+// 1.lenght
+// The length property returns the length of a string:
+// let a= "lenght"
+// a=a.length
+// console.log(a);// output: 6
+
+// 2.CharAt()
+//The charAt() method returns the character at a specified index (position) in a string:
+// let a= "hello"
+// a=a.charAt(0)
+// console.log(a);
+
