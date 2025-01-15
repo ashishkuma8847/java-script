78f01f09663fbfc3453076f160e8ab5f15f4ad62
ashishkuma8847 <ashishkuma8847@gmail.com>
2025-01-15 13:35:02 +0530

    djd

---
+++ react-library/jshook.js
@@ -0,0 +1,9 @@
+// usestae react library ka ek hoo hai jo functional compponents me state manage karne ke liye use hota hai. state ka matlab hai kisi component ke ander ki dynamic data ya values , jo components ke render hone par badal sakti hai .
+//usestate working mkensizem:
+//1.
+// usestate ek array return karta hai jo do cheezein deta hai 
+//first:current state ka value 
+// sec:ek function jo es state ko update karta hai
+
+//syntex:
+//const[state,setst]
